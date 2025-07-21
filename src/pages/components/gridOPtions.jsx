b48f70e158e3d870d73233422d8f32b837f7cea1
Sigma700 <allankirimi65@gmail.com>
2025-07-21 19:26:05 +0300

    added the missing logo

--- src/pages/components/gridOPtions.jsx
+++ src/pages/components/gridOPtions.jsx
@@ -1,26 +1,37 @@
 import React from 'react';
-import { FiCreditCard, FiMail, FiPlus, FiUser, FiUsers } from 'react-icons/fi';
+import { FiBookOpen, FiMail, FiPlus, FiUser, FiUsers } from 'react-icons/fi';
 import { RiLiveFill } from 'react-icons/ri';
 import { CiShare2 } from 'react-icons/ci';
+import { Link } from 'react-router-dom';
 
 const HoverDevCards = () => {
 	return (
 		<div className="p-4">
 			<p className="text-xl font-semibold mb-2">Actions</p>
 			<div className="grid gap-4 grid-cols-2 lg:grid-cols-4">
-				<Card title="Create Timetable" subtitle="Start creating" href="#" Icon={FiPlus} />
-				<Card title="User manual" subtitle="View manual" href="#" Icon={FiMail} />
-				<Card title="Schedule Demo" subtitle="Live guidance session" href="#" Icon={RiLiveFill} />
-				<Card title="Invite Others" subtitle="https://" href="#" Icon={CiShare2} />
+				<Card
+					title="Create Timetable"
+					subtitle="Start creating"
+					path="create-table"
+					Icon={FiPlus}
+				/>
+				<Card title="User manual" subtitle="View manual" path="manual" Icon={FiBookOpen} />
+				<Card
+					title="Schedule Demo"
+					subtitle="Live guidance session"
+					path="demo"
+					Icon={RiLiveFill}
+				/>
+				<Card title="Invite Others" subtitle="https://" path="invite" Icon={CiShare2} />
 			</div>
 		</div>
 	);
 };
 
-const Card = ({ title, subtitle, Icon, href }) => {
+const Card = ({ title, subtitle, Icon, path }) => {
 	return (
-		<a
-			href={href}
+		<Link
+			to={path}
 			className="w-full p-4 rounded border-[1px] border-slate-300 relative overflow-hidden group bg-white"
 		>
 			<div className="absolute inset-0 bg-gradient-to-r from-violet-600 to-indigo-600 translate-y-[100%] group-hover:translate-y-[0%] transition-transform duration-300" />
@@ -33,7 +44,7 @@ const Card = ({ title, subtitle, Icon, href }) => {
 			<p className="text-slate-400 group-hover:text-violet-200 relative z-10 duration-300">
 				{subtitle}
 			</p>
-		</a>
+		</Link>
 	);
 };
 
